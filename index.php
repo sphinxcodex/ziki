3f2205431cccaae9fc0aa60d4f005199e0869ab1
wisdomanthoni <wizitendo10@gmail.com>
2019-04-16 00:25:54 +0100

    ziki start

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Front to the Ziki app. This file doesn't do anything, but bootstraps
+ * Ziki to load the theme.
+ *
+ * @package Ziki
+ */
+/** Loads the Ziki Environment and Theme */
+$app = require( dirname( __FILE__ ) . '/src/bootstrap.php' );
+
