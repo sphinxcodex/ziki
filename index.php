871503193a9150560ed7b2973d0271594a4b6b01
mercyikpe <ikpemercy1@gmail.com>
2019-04-16 19:11:02 +0100

    Route.php added in /src/classes/

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Front to the Ziki app. This file doesn't do anything, but bootstraps
@@ -6,5 +7,15 @@
  * @package Ziki
  */
 /** Loads the Ziki Environment and Theme */
-$app = require( dirname( __FILE__ ) . '/src/bootstrap.php' );
 
+
+$app = require( dirname( __FILE__ ) . '/src/bootstrap.php' );
+$app = require( dirname( __FILE__ ) . '/src/classes/Route.php' );
+
+
+$router = new Router(new Request);
+
+$router->get('/post', function() {
+
+    return (new Post)->get_post();
+});
