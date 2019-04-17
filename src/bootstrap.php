a2f14a5f96bf16ded84c961e7a296a4a013ec1cc
Ogundiji Bolade <kamponistullar@gmail.com>
2019-04-17 11:28:46 +0100

    twig page structured :wrench: (#19)

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,14 @@
 <?php
 // Load our autoloader
 require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/classes/api/Router.php';
+require_once __DIR__.'/classes/api/Request.php';
+
+// Specify our Twig templates location
+$loader = new Twig_Loader_Filesystem(__DIR__.'/../resources/themes/ghost/templates');
+
+// Instantiate our Twig
+$twig = new Twig_Environment($loader);
 
 $logger = new Monolog\Logger('Ziki');
 $logger->pushHandler(new Monolog\Handler\StreamHandler( __DIR__ . DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR . 'app.log'));
