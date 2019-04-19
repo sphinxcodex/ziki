6966e00c88e4325b937deefa835106de28185c87
Elijah Okokon <49094365+DmatrixNg@users.noreply.github.com>
2019-04-19 13:09:27 +0100

    update with session check working  (#58)
    * The blog detail page, responsive and without the comment section (#25)
    
    * added CSS variables for the light theme color mode of Ziki
    
    * colors for the light and dark mode of the Ziki pages
    
    * colors for the light and dark mode of the Ziki project
    
    * responsive blog details page
    
    * new changes
    
    * responsive blog detail page
    
    * did refactor
    
    * u
    
    * make some changes
    
    * -a
    
    * :grin
    
    * setting up setting class
    
    * Merge branch 'master' into development
    
    * update
    
    * Auth page, Setting page, login, logout pages
    
    * Merge branch 'development' into updated

---
+++ classes/class.user.php
@@ -0,0 +1,73 @@
+<?php
+
+Class User {
+
+    private $users;
+
+	function __construct($users){
+
+
+		$this->_users = $users;
+	}
+
+	public function is_logged_in(){
+		if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
+			return true;
+		}
+	}
+
+
+	private function get_user_hash($name){
+
+		try {
+
+			$stmt = $this->_users;
+			$stmt->execute(array('name' => $name));
+
+			return $stmt->fetch();
+
+		} catch(PDOException $e) {
+		    echo '<p class="error">'.$e->getMessage().'</p>';
+		}
+	}
+
+
+	public function login($name,$email,$persist){
+
+
+if ($persist) {
+        $hash = crypt($name.$_SERVER['REMOTE_ADDR']);
+        setcookie('ziki', $name.'#~#'.$hash, time() + 3600 * 24 * 7, '/', '', false, true);
+
+        $_SESSION['loggedin'] = true;
+		    $_SESSION['name'] = $name;
+        $_SESSION['email'] = $email;
+        $_SESSION['accesstoken'] = "local";
+
+      }else {
+        $_SESSION['loggedin'] = true;
+		    $_SESSION['name'] = $name;
+        $_SESSION['email'] = $email;
+        $_SESSION['accesstoken'] ="local";
+
+      }
+
+        return true;
+		}
+
+
+
+	public function logout(){
+
+		session_destroy();
+    // We also unset the cookie
+    if( isset($_COOKIE['pr']) )
+    {
+        setcookie('ziki', '', time() - 3600, '/', '', false, true);
+    }
+	}
+
+}
+
+
+?>
