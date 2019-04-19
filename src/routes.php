1bf760c97ac2fa118b6a7530fa187b2e0626036a
TC Asoluka <austin@silkydev.com.ng>
2019-04-19 04:54:32 +0100

    ý2ý4ý2ý4ý,ý.ý2ý4ý2ý4ý2ý4`ý2ý2ý4ý2ý4ý2ý4dddiý2ý4fisý2ý4aý2ý4I[3~kbkbkbkbkbý2ý4dIkbý2ý4Ifkbpkbý,ý.dkbpkbý,ý.2pkbkbkbkbý2ý4ý2ý4:rkukukukukuku[3~kbkbkbkbkbkbvim sleek_c.mgkbdkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkDkDkDkDkDkDkDkDkDkDkDkDkDkDkDkDkDkDkdkdkdkdkdkdkbIIIHkbkbkbkbkb:wddvimkdkukuklkr:wq:qQkukuý2ý4dddddý,ý.vim sleekkbkbkbkbkbvim sleek_c.ngkbkbkb\kbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbý,ý.:q+kukdkdklklkukl`:wq:w1kbqy:wqddfdfklkdkdkdPS[201~ý,ý.dý,ý.kukdkdkdkdkukukukukukukukdkdý,ý.ý,ý.wkb:wqkukdkdkdklklklkdkdkrkrkukukudskdkdkdkdkdcvklklkdklklklkrkrkrklklklkukuklklý,ý.kukukukdkdý,ý.dkbkb:wkuklkrkrkdkdkukdssAkbkbkbAIkbIkrkrkrOOkbý,ý.krkrkrkrkukuklpý,ý.pý,ý.ýKýKýKýKýKýKýKýKýKýKýKýKýKýKýKýKýKýKýKýKý,ý.skbtkbstý,ý.ý,ý.stkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkr:wkbsaveas file:closeýJýJýJýJýJýJýJýJýJýJýJýJýJýJýKýKýKýKýKýKýKýKýKýKýKýJýJýJýJýKýKýKýKýKýKýKýKýKýKýKýK:wq (#49)

--- src/routes.php
+++ src/routes.php
@@ -58,3 +58,7 @@ $router->get('/following', function($request) {
 $router->get('/followers', function($request) {
     return $this->template->render('followers.html');
 });
+
+$router->get('/df_editor', function($request) {
+    return $this->template->render('df_editor.html');
+});
