da6be6e9a6e2776b06d0dc08e8698070d494d166
Ogundiji Bolade Adio <kamponistullar@gmail.com>
2019-04-20 10:09:06 +0100

    added collection class to handle document matching certain criterias :wrench:

--- src/core/collection.php
+++ src/core/collection.php
@@ -7,5 +7,38 @@ namespace Ziki\Core;
 
 class Collection
 {
+    protected $dir;
+    
+    protected $entries;
 
+    public function __construct($dir, $entries)
+    {
+        $this->dir     = $dir;
+        $this->entries = $entries;
+    }
+    
+    public function slug()
+    {
+        return $this->dir->getBasename();
+    }
+    
+    public function entries()
+    {
+        return $this->entries;
+    }
+    
+    public function title()
+    {
+        $title = ltrim($this->slug(), '/');
+        $title = str_replace(['-', '_'], ' ', $title);
+        $title = ucwords($title);
+        return $title;
+    }
+    
+    public function meta()
+    {
+        return [
+            'title' => $this->title(),
+        ];
+    }
 }
