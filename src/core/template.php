278c4d4279bbfd8c8564bdfc76a1efe712da04ae
chibuikeIg <43254308+chibuikeIg@users.noreply.github.com>
2019-04-20 23:16:34 +0100

    Working (#91)
    * added a few config for twig caching
    
    * corrected an invalid control statements

--- src/core/template.php
+++ src/core/template.php
@@ -6,17 +6,36 @@ use Twig\Loader\FilesystemLoader;
 class Template
 {
     private $twig;
+    
 
     public function __construct($templatePath) 
     {
-        $this->twig = new Environment( new FilesystemLoader($templatePath),[
-            'auto_reload' => true,
-            'cache' => ZIKI_BASE_PATH.'/storage/cache/views',
-        ]);
+
+        $this->twig = new Environment( new FilesystemLoader($templatePath),$this->setTwigCaching());
     }
    
     public function render($page, array $parameters = [])
     {
         return $this->twig->render($page, $parameters);
+    }
+
+    private function setTwigCaching()
+    {
+        $data=[];
+
+        if (ZIKI_PROD){
+            $data = [
+                        'auto_reload' => true,
+                        'cache' => ZIKI_BASE_PATH.'/storage/cache/views',
+                    ];
+            }
+            else
+            {
+                $data = [
+                    'auto_reload' => true,
+                    'cache' => false,
+                ];
+            }
+        return $data;
     }
 }
