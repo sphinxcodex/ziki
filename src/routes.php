91f090a14eddd1f34e4bf13e845806b17eefe76e
= <=>
2019-04-24 17:30:17 +0100

    patch, still WIP :wrench:

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,9 @@
 <?php
 
 $router->get('/', function($request) {
+    $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] .  $_SERVER['REQUEST_URI'];
+    $installed_address = SITE_URL;
+    $site_address = ($link == $installed_address) ? $installed_address : die("Permittion denied for this domain, please use a new installation for a new domain");
     $directory = "./storage/contents/";
     $ziki = new Ziki\Core\Document($directory);
     $post = $ziki->get();
@@ -75,4 +78,22 @@ $router->get('/404', function($request) {
   });
   $router->get('/about', function($request) {
     return $this->template->render('about-us.html');
+  });
+  $router->get('/login/google', function($request){
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://auth.techteel.com/login/google'
+    ]);
+    $result = curl_exec($curl);
+    return $result;
+  });
+  $router->get('/login/google/callback', function($request){
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://auth.techteel.com/login/facebook'
+    ]);
+    $result = curl_exec($curl);
+    return $result;
   });
