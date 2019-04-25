5b1beec31f9897daafeed99c895109da3540fe02
wisdomanthoni <wizitendo10@gmail.com>
2019-04-25 22:49:52 +0100

    format core files document

--- src/core/subscribe.php
+++ src/core/subscribe.php
@@ -6,10 +6,10 @@ namespace Ziki\Core;
  */
 class Subscribe
 {
- var $name;
- var $rss;
- var $img;
- var $desc;
+  var $name;
+  var $rss;
+  var $img;
+  var $desc;
 
   public function setSubName($value)
   {
@@ -27,63 +27,58 @@ class Subscribe
   {
     $this->img = $value;
   }
-public function follow($db)
-{
-              //Saving new post
-              $db = "C:/Users/user/ziki-1/storage/rss/subscriber.json";
-              $file = file_get_contents($db, true);
-              $data=json_decode($file,true);
-              unset($file);
-              foreach ($data as $key => $value) {
+  public function follow($db)
+  {
+    //Saving new post
+    $db = "C:/Users/user/ziki-1/storage/rss/subscriber.json";
+    $file = file_get_contents($db, true);
+    $data = json_decode($file, true);
+    unset($file);
+    foreach ($data as $key => $value) {
 
-                 if ($value["name"] == $this->name) {
-                  // $message="Your have already Subscribed to this channel";
+      if ($value["name"] == $this->name) {
+        // $message="Your have already Subscribed to this channel";
 
-              return false;
-            }else {
-              return true;
-            }
-              };
-              if (true) {
+        return false;
+      } else {
+        return true;
+      }
+    };
+    if (true) {
 
-              $time = date("Y-m-d h:i:sa");
+      $time = date("Y-m-d h:i:sa");
 
-                  $img = $this->img;
-                  $sub[] = array('name'=> $this->name, 'rss'=>$this->rss,'desc'=>$this->desc, 'img'=> $this->img, 'time' => $time);
-                  $json_db = "C:/Users/user/ziki-1/storage/rss/subscriber.json";
-                  $prev_sub = json_decode($db);
+      $img = $this->img;
+      $sub[] = array('name' => $this->name, 'rss' => $this->rss, 'desc' => $this->desc, 'img' => $this->img, 'time' => $time);
+      $json_db = "C:/Users/user/ziki-1/storage/rss/subscriber.json";
+      $prev_sub = json_decode($db);
 
-                  $new =array_merge($sub, $prev_sub);
-                  $fp = fopen($json_db, 'w') or die("post DB not found");
-                  //die(json_encode($new));
-                  $new_sub = fwrite($fp, json_encode($new));
-                  fclose($fp);
+      $new = array_merge($sub, $prev_sub);
+      $fp = fopen($json_db, 'w') or die("post DB not found");
+      //die(json_encode($new));
+      $new_sub = fwrite($fp, json_encode($new));
+      fclose($fp);
 
-                  return true;
-                }else {
-                return false;
-                }
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public function unfollow($del)
+  {
+    $db = "storage/contents/subscriber.json";
+    $file = file_get_contents($db, true);
+    $data = json_decode($file, true);
+    unset($file);
+    foreach ($data as $key => $value) {
 
+      if ($value["name"] == $del) {
+        unset($data[$key]);
+      }
+    };
 
-
-}
-public function unfollow($del)
-{
-            $db = "storage/contents/subscriber.json";
-            $file = file_get_contents($db, true);
-            $data=json_decode($file,true);
-            unset($file);
-           foreach ($data as $key => $value) {
-
-               if ($value["name"] == $del) {
-              unset($data[$key]);
-              }
-           };
-
-            $result=json_encode($data);
-            file_put_contents($db, $result);
-            unset($result);
-          }
-
-
+    $result = json_encode($data);
+    file_put_contents($db, $result);
+    unset($result);
+  }
 }
