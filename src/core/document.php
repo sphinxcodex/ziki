c1acb9f00d58f2efa4633e56dc11b43f8554f628
devmohy <yayahmohammed@gmail.com>
2019-04-27 17:56:22 +0100

    Implement draft

--- src/core/document.php
+++ src/core/document.php
@@ -295,12 +295,86 @@ class Document
             return $posts;
         }
     }
-    //Save Draft
-    public function createDraft()
-    { }
-    //Get Draft
-    public function getDraft()
-    { }
+     /**
+     * This allows you to save posts in draft
+     * 
+     */
+     //for creating markdown files
+     public function createDraft($title, $content,$tags)
+     {
+        $time = date("F j, Y, g:i a");
+        $unix = strtotime($time);
+        // Write md file
+        $document = FrontMatter::parse($content);
+        $md = new Parser();
+        $markdown = $md->parse($document);
+
+        $yaml = $markdown->getYAML();
+        $html = $markdown->getContent();
+        $doc = FileSystem::write($this->file, $yaml . "\n" . $html);
+
+        $yamlfile = new Doc();
+        $yamlfile['title'] = $title;
+        $tag = explode(",",$tags);
+        $put = [];
+        foreach($tag as $value){
+            array_push($put,$value);
+        }
+        $yamlfile['tags'] = $put;
+        $yamlfile['post_dir'] = SITE_URL . "/storage/contents/{$unix}";
+        $striped = str_replace(' ', '-', $title);
+        $yamlfile['slug'] = $striped."-{$unix}";
+        $yamlfile['timestamp'] = $time;
+        $yamlfile->setContent($content);
+        $yaml = FrontMatter::dump($yamlfile);
+        $file = $this->file;
+        $dir = $file . $unix . ".md";
+        //return $dir; die();
+        $doc = FileSystem::write($dir, $yaml);
+
+        if ($doc) {
+            $result = array("error" => false, "message" => "Draft saved successfully");
+        } else {
+            $result = array("error" => true, "message" => "Fail while publishing, please try again");
+        }
+        return $doc;
+     }
+     //get post
+     public function getDraft()
+     {
+         $finder = new Finder();
+ 
+         // find all files in the current directory
+         $finder->files()->in($this->file);
+         $posts = [];
+         if ($finder->hasResults()) {
+             foreach ($finder as $file) {
+                 $document = $file->getContents();
+                 $parser = new Parser();
+                 $document = $parser->parse($document);
+                 $yaml = $document->getYAML();
+                 $body = $document->getContent();
+                 //$document = FileSystem::read($this->file);
+                 $parsedown  = new Parsedown();
+                 $title = $parsedown->text($yaml['title']);
+                 $slug = $parsedown->text($yaml['slug']);
+                 $slug = preg_replace("/<[^>]+>/", '', $slug);
+                 $bd = $parsedown->text($body);
+                 $time = $parsedown->text($yaml['timestamp']);
+                 $url = $parsedown->text($yaml['post_dir']);
+                 $content['title'] = $title;
+                 $content['body'] = $bd;
+                 $content['url'] = $url;
+                 $content['slug'] = $slug;
+                 $content['timestamp'] = $time;
+                 array_push($posts, $content);
+             }
+             return $posts;
+         } else {
+             return false;
+         }
+     }
+
     // post
     public function update()
     { }
