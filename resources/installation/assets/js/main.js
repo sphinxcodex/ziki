283355d85271e7b3d7fff631ecaa19f10eaeecb9
Ogundiji Bolade Adio <kamponistullar@gmail.com>
2019-05-01 07:58:52 +0100

    social login for installations :smile: :fire: :ambulance:

--- resources/installation/assets/js/main.js
+++ resources/installation/assets/js/main.js
@@ -5,9 +5,27 @@ window.onload = function() {
     event.preventDefault();
   });
   // handle form switching
+  const $ = document.querySelector.bind(document)
   let steps = document.getElementsByClassName("step-form");
   let stepOneForm = steps[0];
   let stepTwoForm = steps[1];
+  $('#installButton').addEventListener("click", function(){
+    let fname = $('#firstname').value;
+    let lname = $('#lastname').value;
+    let uname = $('#username').value;
+    let mail = $('.e-mail').value;
+    let dnn = $('.domain-name').value;
+    let dn = $('.domain').value;
+    let tag = $('.tag').value;
+    let country = $('.country').value;
+
+    fetch('/publish', {
+      method: 'POST',
+      body: JSON.stringify({firstname:fname, lastname:lname, username:uname, email:mail, url:`${dnn+dn}`, bio:tag, country:country})
+    })
+    .then((res) => { console.log(res) })
+    .catch((err)=>console.log(err))
+  });
   document.getElementById("nextButton").addEventListener("click", function() {
     // stepOneForm.classList.add("animated", "fadeOutLeft");
     stepOneForm.style.display = "none";
--- src/config/routes.php
+++ src/config/routes.php
@@ -6,7 +6,7 @@ Router::get('/about/{id}', function($request,$id) {
 });
 Router::get('/', function($request) {
     $user = new Ziki\Core\Auth();
-    if ($user::isInstalled() == true) {
+    if ($user::isInstalled() == false) {
         return $user->redirect('/install');
     }
     else{
@@ -217,13 +217,24 @@ Router::get('/download', function($request) {
     return $this->template->render('download.html');
 });
 Router::get('/auth/{provider}/{token}', function($request, $token){
+    $param = $request->getBody();
     $user = new Ziki\Core\Auth();
     $check = $user->validateAuth($token);
     if($_SESSION['login_user']['role'] == 'guest'){
         return $user->redirect('/');
     }
     else{
-        return $user->redirect('/timeline');
+        return $user->redirect('/settings');
+    }
+});
+Router::get('/setup/{provider}/{token}', function($request, $token){
+    $user = new Ziki\Core\Auth();
+    $check = $user->validateAuth($token);
+    if($_SESSION['login_user']['role'] == 'guest'){
+        return $user->redirect('/');
+    }
+    else{
+        return $user->redirect('/profile');
     }
 });
 Router::get('/logout', function($request) {
@@ -239,6 +250,15 @@ Router::post('/api/upload-image', function() {
 });
 
 Router::get('/install', function($request) {
-    return $this->installer->render('install.html');
+    $user = new Ziki\Core\Auth();
+    $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+    $host = $user->hash($url);
+    return $this->installer->render('install.html', ['host' => $host]);
+});
+
+Router::post('/setup', function($request) {
+    $data = $request->getBody();
+    $user = new Ziki\Core\Auth();
+    die(json_encode($data));
 });
 
